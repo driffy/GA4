a107156c471fe331ef90947fe8bddaa3c3679bad
driffy <zzpoer@gmail.com>
2022-08-27 23:51:10 +0900

    Create 스크롤_이벤트.sql

---
+++ 스크롤_이벤트.sql
@@ -0,0 +1,7 @@
+SELECT
+    traffic_source.source as source,
+    count(case when event_name="scroll" then 1 end) as scroll
+  FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+  where _TABLE_SUFFIX BETWEEN '20210117' AND '20210123'
+  group by source
+  having scroll > 0
