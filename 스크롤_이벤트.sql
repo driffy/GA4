87b6a09313b5238a52a547634afa04b441a0589b
driffy <zzpoer@gmail.com>
2023-05-25 17:12:36 +0900

    Update 스크롤_이벤트.sql

--- 스크롤_이벤트.sql
+++ 스크롤_이벤트.sql
@@ -4,5 +4,5 @@ SELECT
     count(case when event_name="scroll" then 1 end) as scroll
   FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
   where _TABLE_SUFFIX BETWEEN '20210117' AND '20210123'
-  group by source
+  group by source, event_date
   having scroll > 0
